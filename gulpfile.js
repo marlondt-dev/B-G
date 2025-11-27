10596cca47ef304fb9ac717a2dcdef0e3109658e
Marlon Torres <marlon.dt@hotmail.com>
2025-11-27 11:35:02 +0100

    feat(gulp): created and modified gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,74 @@
-const { src, dest, watch, series, parallel } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('autoprefixer');
-const postcss = require('gulp-postcss')
-const sourcemaps = require('gulp-sourcemaps')
-const cssnano = require('cssnano');
-const concat = require('gulp-concat');
-const terser = require('gulp-terser-js');
-const rename = require('gulp-rename');
-const imagemin = require('gulp-imagemin'); // Minificar imagenes 
-const notify = require('gulp-notify');
-const cache = require('gulp-cache');
-const clean = require('gulp-clean');
-const webp = require('gulp-webp');
-
+const { src, dest, watch, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("autoprefixer");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+const cssnano = require("cssnano");
+const concat = require("gulp-concat");
+const terser = require("gulp-terser-js");
+const rename = require("gulp-rename");
+const sharp = require("sharp");
+const fs = require("fs");
+const path = require("path");
+const { glob } = require("glob");
+ 
 const paths = {
-    scss: 'src/scss/**/*.scss',
-    js: 'src/js/**/*.js',
-    imagenes: 'src/img/**/*'
-}
-
+  scss: "src/scss/**/*.scss",
+  js: "src/js/**/*.js",
+  imagenes: "src/img/**/*.{png,jpg,jpeg}",
+};
+ 
 function css() {
-    return src(paths.scss)
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        // .pipe(postcss([autoprefixer()]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('build/css'));
+  return src(paths.scss)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("build/css"));
 }
-
+ 
 function javascript() {
-    return src(paths.js)
-      .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js'))
-      .pipe(terser())
-      .pipe(sourcemaps.write('.'))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'))
+  return src(paths.js)
+    .pipe(sourcemaps.init())
+    .pipe(concat("bundle.js"))
+    .pipe(terser())
+    .pipe(sourcemaps.write("."))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(dest("./build/js"));
 }
-
-function imagenes() {
-    return src(paths.imagenes)
-        .pipe(cache(imagemin({ optimizationLevel: 3 })))
-        .pipe(dest('build/img'))
-        .pipe(notify('Imagen Completada' ));
+ 
+async function imagenes(done) {
+  const srcDir = "src/img";
+  const buildDir = "build/img";
+  const images = await glob("src/img/**/*.{png,jpg,jpeg}");
+ 
+  images.forEach((file) => {
+    const relativePath = path.relative(srcDir, path.dirname(file));
+    const outputSubDir = path.join(buildDir, relativePath);
+    procesarImagenes(file, outputSubDir);
+  });
+  done();
 }
-
-function versionWebp() {
-    return src(paths.imagenes)
-        .pipe(webp())
-        .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Imagen Completada' }));
+ 
+function procesarImagenes(file, outputSubDir) {
+  if (!fs.existsSync(outputSubDir)) {
+    fs.mkdirSync(outputSubDir, { recursive: true });
+  }
+  const baseName = path.basename(file, path.extname(file));
+  const outputFile = path.join(outputSubDir, `${baseName}.jpg`);
+  const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`);
+ 
+  const options = { quality: 80 };
+  sharp(file).jpeg(options).toFile(outputFile);
+  sharp(file).webp(options).toFile(outputFileWebp);
 }
-
-
+ 
 function watchArchivos() {
-    watch(paths.scss, css);
-    watch(paths.js, javascript);
-    watch(paths.imagenes, imagenes);
-    watch(paths.imagenes, versionWebp);
+  watch(paths.scss, css);
+  watch(paths.js, javascript);
+  watch(paths.imagenes, imagenes);
 }
-
+ 
 exports.css = css;
 exports.watchArchivos = watchArchivos;
-exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos); 
+exports.default = parallel(css, javascript, imagenes, watchArchivos);
